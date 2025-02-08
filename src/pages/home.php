7c0074fa6e8b18d185f6c548688c16673da7717d
Your Name <you@example.com>
2025-02-08 22:46:56 +0700

    up

--- src/pages/home.php
+++ src/pages/home.php
@@ -11,6 +11,20 @@ $stmt->execute();
 while ($row = $stmt->fetch()) {
     array_push($users, $row);
 }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = FormHelper::get_input("username");
+    $email = FormHelper::get_input("email");
+    $password = FormHelper::get_input("password");
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $stmt = $conn->prepare("INSERT INTO `users` (`username`, `password`, `email`, `created_at`) VALUES (?, ?, ?, current_timestamp())");
+
+    $stmt->bindParam(1, $username);
+    $stmt->bindParam(2, $password_hash);
+    $stmt->bindParam(3, $email);
+    $stmt->execute();
+    URLHelper::redirect(URLHelper::my_url());
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,41 +36,51 @@ while ($row = $stmt->fetch()) {
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 
-<body>
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Pass</th>
-                <th>Created At</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($users as $user): ?>
-                <tr>
-                    <td>
-                        <?php echo $user["id"] ?>
-                    </td>
-                    <td><?php echo $user["username"] ?></td>
-                    <td><?php echo $user["email"] ?></td>
-                    <td><?php echo $user["password"] ?></td>
-                    <td><?php echo $user["created_at"] ?></td>
+<body class="p-5">
+    <div class="flex justify-center items-center">
+        <table class="border">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                <tr class="">
+                    <th scope="col" class="px-6 py-3">Id</th>
+                    <th scope="col" class="px-6 py-3">Username</th>
+                    <th scope="col" class="px-6 py-3">Email</th>
+                    <th scope="col" class="px-6 py-3">Pass</th>
+                    <th scope="col" class="px-6 py-3">Created At</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <div>
-        <?php if (empty($users)): ?>
-            <p>
-                No data
-            </p>
-        <?php else: ?>
-            <p>
-                Have data
-            </p>
-        <?php endif; ?>
+            </thead>
+            <tbody class="">
+                <?php foreach ($users as $user): ?>
+                    <tr class="bg-white border-b border-gray-200">
+                        <td class="px-6 py-4">
+                            <?php echo $user["id"] ?>
+                        </td>
+                        <td class="px-6 py-4"><?php echo $user["username"] ?></td>
+                        <td class="px-6 py-4"><?php echo $user["email"] ?></td>
+                        <td class="px-6 py-4"><?php echo $user["password"] ?></td>
+                        <td class="px-6 py-4"><?php echo $user["created_at"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="flex flex-col justify-center items-center mt-10">
+        <div class="w-96 p-5 rounded-lg shadow-lg hover:shadow-xl bg-gray-50">
+            <h1>Add User</h1>
+            <form action="<?php echo URLHelper::my_url() ?>" method="post" class="space-y-3 ">
+                <div>
+                    <input type="text" name="username" id="username" placeholder="Username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                </div>
+                <div>
+                    <input type="text" name="email" id="email" placeholder="Email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                </div>
+                <div>
+                    <input type="password" name="password" id="password" placeholder="Password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                </div>
+                <div>
+                    <button type="submit" class="px-2 py-1 bg-blue-500 hover:bg-blue-600 rounded-lg w-full text-white transition-all">Submit</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
